967f9cf0a0c22ef4018cd24d0565a8841d470546
Darius Capizzi and Joey Arnstein <joey@>
2016-04-21 16:31:03 -0700

    More functionality

--- src/test/java/CountCountTest.java
+++ src/test/java/CountCountTest.java
@@ -7,7 +7,7 @@ public class CountCountTest {
   @Test
   public void count_CorrispondingStringOfNum_String(){
     CountCount testCount = new CountCount();
-    assertEquals("$1 and 1 cents.", testCount.CountCents("226"));
+    assertEquals("$1 and 1 cents.", testCount.CountCents("206"));
   }
 
 }
